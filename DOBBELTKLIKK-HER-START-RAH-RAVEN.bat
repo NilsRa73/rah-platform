ea2fa24cc9bddcf8d093ba83357303b29c039a61
NilsRa73 <nils.ravnbo@gmail.com>
2026-08-06 07:33:41 +0200

    Make existing Raven start button update and launch v1.7

--- DOBBELTKLIKK-HER-START-RAH-RAVEN.bat
+++ DOBBELTKLIKK-HER-START-RAH-RAVEN.bat
@@ -1,3 +1,29 @@
 @echo off
+setlocal EnableExtensions
 cd /d "%~dp0"
-call "START-RAH-RAVEN-V2.bat"
+title RAH Raven - Update and Start
+
+set "UPDATER=%~dp0UPDATE-RAH-RAVEN.ps1"
+set "UPDATER_URL=https://raw.githubusercontent.com/NilsRa73/rah-platform/main/UPDATE-RAH-RAVEN.ps1"
+
+echo.
+echo  RAH RAVEN - UPDATE AND START
+echo  =============================
+echo.
+echo Henter siste godkjente Raven-filer fra NilsRa73/rah-platform...
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing -Uri '%UPDATER_URL%' -OutFile '%UPDATER%.download'; if((Get-Item '%UPDATER%.download').Length -lt 1){throw 'Updater download was empty'}; Move-Item -Force '%UPDATER%.download' '%UPDATER%'"
+if errorlevel 1 goto :error
+
+powershell -NoProfile -ExecutionPolicy Bypass -File "%UPDATER%"
+if errorlevel 1 goto :error
+exit /b 0
+
+:error
+echo.
+echo FEIL: Raven kunne ikke oppdateres eller startes.
+echo Eksisterende filer er ikke med vilje slettet.
+echo Se rah-raven-update.log i denne mappen.
+echo.
+pause
+exit /b 1
