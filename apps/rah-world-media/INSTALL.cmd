b1f4f9fff4b44d7f3c534266ec93ec5fe4b1268e
NilsRa73 <nils.ravnbo@gmail.com>
2026-09-23 16:09:19 +0200

    World Media v12: promote INSTALL.cmd from published package

--- apps/rah-world-media/INSTALL.cmd
+++ apps/rah-world-media/INSTALL.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-title RAH World Media 9.9 SUPER MODE - INSTALL
+title RAH World Media 12.0 RAVEN SYNC DECK - INSTALL
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0RAH_INSTALL.ps1"
 exit /b %errorlevel%
