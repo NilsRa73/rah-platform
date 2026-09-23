ce9849cf5966430a26e0a967e86650ec7f791be7
NilsRa73 <nils.ravnbo@gmail.com>
2026-09-23 11:25:26 +0200

    World Media 9.9.1: update main launcher title

--- apps/rah-world-media/START-HER.cmd
+++ apps/rah-world-media/START-HER.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-title RAH World Media v9.9 SUPER MODE
+title RAH World Media v9.9.1 SMART PREVIEW
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0RAH_BOOTSTRAP.ps1"
 exit /b %errorlevel%
