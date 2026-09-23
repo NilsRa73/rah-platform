9373279f18ab5bf3296481504642c1a08daa8db0
NilsRa73 <nils.ravnbo@gmail.com>
2026-09-23 11:25:29 +0200

    World Media 9.9.1: update installer title

--- apps/rah-world-media/INSTALL.cmd
+++ apps/rah-world-media/INSTALL.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-title RAH World Media 9.9 SUPER MODE - INSTALL
+title RAH World Media 9.9.1 SMART PREVIEW - INSTALL
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0RAH_INSTALL.ps1"
 exit /b %errorlevel%
