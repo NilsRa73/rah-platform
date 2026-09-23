67541ca1145b6ecbb7b6ddb5d1f0a9891f05920d
NilsRa73 <nils.ravnbo@gmail.com>
2026-09-23 11:29:07 +0200

    World Media: promote published v11 canonical source

--- apps/rah-world-media/START-HER.cmd
+++ apps/rah-world-media/START-HER.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-title RAH World Media v9.9 SUPER MODE
+title RAH World Media v11.0 RAVEN BROADCAST DECK
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0RAH_BOOTSTRAP.ps1"
 exit /b %errorlevel%
