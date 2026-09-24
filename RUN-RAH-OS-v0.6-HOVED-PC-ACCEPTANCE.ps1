39608f838b222914972d17d18c7c436d09d9c88f
NilsRa73 <nils.ravnbo@gmail.com>
2026-09-24 23:39:59 +0200

    fix(rah-os): avoid PowerShell List object binder crash

--- RUN-RAH-OS-v0.6-HOVED-PC-ACCEPTANCE.ps1
+++ RUN-RAH-OS-v0.6-HOVED-PC-ACCEPTANCE.ps1
@@ -95,7 +95,7 @@ if($SelfTest){
 }
 
 New-Item -ItemType Directory -Force -Path $script:StateRoot | Out-Null
-$stages = New-Object System.Collections.Generic.List[object]
+$stages = [System.Collections.Generic.List[object]]::new()
 
 # 1) FRONT DOOR
 $front = Find-RahFile 'RAH-OS-SELFTEST.ps1'
