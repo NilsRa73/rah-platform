79e8f2f0ef83eb4337eeea7aac3d0fdaa42fda7c
NilsRa73 <nils.ravnbo@gmail.com>
2026-09-25 01:51:24 +0200

    Update World Media packaging for v14 Smart Cluster

--- apps/rah-world-media/START-HER.cmd
+++ apps/rah-world-media/START-HER.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-title RAH World Media v12.0 RAVEN SYNC DECK
+title RAH World Media v14.0 RAVEN SMART CLUSTER
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0RAH_BOOTSTRAP.ps1"
 exit /b %errorlevel%
